03d610626daf9a34febdf3d9131d6dd2d776759b
Ronny Abraham <ronny.coder @gmail.com>
2017-12-26 12:02:00 +0200

    add CodingKey/Decoder init method

--- search.playground/Contents.swift
+++ search.playground/Contents.swift
@@ -13,7 +13,35 @@ struct StoreItems: Codable {
     var kind: String
     var artworkURL: URL?
     
+    var description
     
+    enum CodingKeys: String, CodingKey {
+        case name = "trackName"
+        case artist = "artistName"
+        case kind
+        case artworkURL = "artistUrl100"
+    }
+    
+    enum AdditionalKeys: String, CodingKey {
+        case longDescription
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+    
+        name = try values.decode(String.self, forKey: CodingKeys.name)
+        artist = try values.decode(String.self, forKey: CodingKeys.name)
+        kind = try values.decode(String.self, forKey: CodingKeys.name)
+        artworkURL = try values.decode(String.self, forKey: CodingKeys.name)
+
+        if let description = try? values.decode(String.self, forKey: CodingKeys.description) {
+            self.description = description
+        } else {
+            let additionalValues = try decoder.container(keyedBy: AdditionalKeys.self)
+
+            description = (try? additionalValues.decode(String.self, forKey: AdditionalKeys.longDescription)) ?? ""
+        }
+    }
     
 
     init?(json: [String: Any]) {
